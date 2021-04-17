fdd8af723806c9ca0e816365eeb7a469b4f08d0b
FrankSolleveld <fsolleveld@outlook.com>
2021-04-17 14:28:08 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Frank Solleveld on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
