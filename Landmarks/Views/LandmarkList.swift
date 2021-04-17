23c96eff577145a0670d4501e2c6812e2bce5ea7
FrankSolleveld <fsolleveld@outlook.com>
2021-04-17 16:21:10 +0200

    dynamically loading in the landmarks

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Frank Solleveld on 17/04/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
